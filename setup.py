03a061634e35e2bdbce44f3b486b7b439a0b8def
Fatih <fatih.bostanci@hotmail.de>
2020-03-01 10:11:01 +0100

    created package scaffolding

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py_inforce',
+      version='0.1',
+      description='Optimal Sequential Decision Algorithms in Python',
+      url='https://github.com/Mufabo/py_inforce',
+      author='M. Fatih Bostanc',
+      author_email='fatih.bostanci@hotmail.de',
+      license='MIT',
+      packages=['py_inforce'],
+      zip_safe=False)
